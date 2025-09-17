1240bcf44e3df90cf4dfb0f60153532b104188fc
ada-e29361b312 <yueyujiang20@163.com>
2025-09-17 21:50:07 +0800

    Update j-test.py

--- j-test.py
+++ j-test.py
@@ -1,6 +1,14 @@
-a = 1
-b = 4
-c=3
-d=17
+import numpy as np
+import numpy.linalg as nply
 
-print((a+b-(c**4)) / d)
+
+
+from scipy.sparse import diags
+
+n = 10
+
+vals = np.array([-np.ones(n-1), 2*np.ones(n), -np.ones(n-1)]) #/ dx**2
+A = diags(vals, [-1, 0, 1])
+
+print(vals)
+print(A.toarray())
