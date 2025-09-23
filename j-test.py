856622992df53697dbd1b1f68e66fbb2fb6c502b
ada-e29361b312 <yueyujiang20@163.com>
2025-09-23 19:18:04 +0800

    Update j-test.py

--- j-test.py
+++ j-test.py
@@ -1,14 +1,63 @@
-import numpy as np
-import numpy.linalg as nply
+
+import random
+import matplotlib.pyplot as plt
+import math
+
+class Alien:
+    
+    def __init__(self):
+        self.age = 1
+        self.isAlive = True
+        
+    def birthday(self):
+        self.age+=1
+        
+    def considerDying(self):
+        if random.randint(0,10) < self.age:
+            self.isAlive = False
+            
+    def reproduce(self):
+        return self.isAlive and random.randint(0,6) > self.age
+
+
+aliens = [Alien()]
+timeSeries = []
+            
+for i in range(100):
+
+    
+    aliensToAdd = 0
+    
+    for alien in aliens:
+        alien.birthday()
+        if alien.reproduce():
+            aliensToAdd+=1
+        alien.considerDying()
+        
+    aliens = [x for x in aliens if x.isAlive]
+    
+    for new in range(aliensToAdd):
+        aliens.append(Alien())
+        
+    timeSeries.append(len(aliens))
+    
+x = list(range(100))
+y = timeSeries
 
 
 
-from scipy.sparse import diags
 
-n = 10
 
-vals = np.array([-np.ones(n-1), 2*np.ones(n), -np.ones(n-1)]) #/ dx**2
-A = diags(vals, [-1, 0, 1])
+import math
 
-print(vals)
-print(A.toarray())
+def perimeter_calc(radius):
+    try:
+        assert isinstance(radius, (int, float)) and radius >= 0
+        return math.pi * 2 * radius
+    except AssertionError:
+        print("invalid input")
+         # return explicitly
+    
+
+print(perimeter_calc('l'))  # prints "invalid input" and returns None
+print(perimeter_calc(5))    # 31.41592653589793
